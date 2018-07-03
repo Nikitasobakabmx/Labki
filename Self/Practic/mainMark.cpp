c96dcec0d429760850014b8549a4dfc08df4e596
Nikitossii <nikitasobakabmx@gmail.com>
2018-07-03 15:05:22 -0700

    the lastest

--- Self/Practic/mainMark.cpp
+++ Self/Practic/mainMark.cpp
@@ -5,7 +5,7 @@ int main(){
     float x,y;
     printf("Input x and y\n");
     scanf("%f%f",&x,&y);
-    if(((exp(x)-1)<y) && (x > 0) && (x < 2.5) && (y > 0.5))
+    if(((exp(x)-1) > y) && (x > 0) && (x < 2.5) && (y > 0))
         printf("Point (%.2f,%.2f) in graphic\n",x,y);
     else 
         printf("Point (%.2f,%.2f) out of graphic\n",x,y);
