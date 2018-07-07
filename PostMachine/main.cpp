1b9c5e7a9a8970554a1951c840ea771b47504350
Nikita <Nikitasobakabmx@gmail.com>
2018-07-07 16:16:24 -0700

    dddd

---
+++ PostMachine/main.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <fstream>
+void toCode(std::ifstream &file, char *string){
+    
+}
+
+int main(int argv, char **argv){
+    if(argv != 3){
+        printf("Erorr!\nYou mast input 2 files!");
+        return 0;
+    }
+
+    return 0;
+}
