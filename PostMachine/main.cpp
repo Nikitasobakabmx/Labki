1274e1a822b5f75d60b9c07e2bdf616ab9f2f018
Nikita <nikitasobakabmx@gmail.com>
2018-07-08 15:08:29 +0300

    оо

--- PostMachine/main.cpp
+++ PostMachine/main.cpp
@@ -1,7 +1,30 @@
 #include <stdio.h>
 #include <fstream>
-void toCode(std::ifstream &file, char *string){
+void toCode(std::ofstream &file, char *string){
+    file << "#include <stdio.h>\n#include <fstream>\nint main(){\n"
+    std::string line = "line" + string[0];
+    file << line;
     
+    switch (string[1])
+    {
+        case '-':
+            
+            break;
+        case '?':
+            /* code */
+            break;
+        case '<':
+            /* code */
+            break;
+        case 'V':
+            /* code */
+            break;
+        case '!':
+            /* code */
+            break;
+        default:
+            break;
+    }
 }
 
 int main(int argv, char **argv){
