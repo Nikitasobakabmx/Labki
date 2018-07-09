8b3142c0810d31e112344b16363fb1f9f5d42521
Nikita <nikitasobakabmx@gmail.com>
2018-07-09 16:11:39 +0300

    .

--- Self/Practic/house.cpp
+++ Self/Practic/house.cpp
@@ -1,2 +1,23 @@
 #include <doski>
 #include <kirpichi>
+
+struct home{
+    int kirpichi;
+    int doski;
+    float divan;
+    char *telek;
+};
+
+struct home build(struct home dom){
+    dom.kirpichi = 100;
+    dom.doski = 200;
+    dom.divan = 150;
+    dom.telek = "smotry";
+    return dom;
+}
+
+int main(){
+    struct home dom;
+    dom = build(dom);
+    return 0;
+}
